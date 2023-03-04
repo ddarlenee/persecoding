1fa66cf9f5515fc9c0d73f6cfbf62f57241dc1ad
ddarlenee <125565155+ddarlenee@users.noreply.github.com>
2023-03-04 16:41:26 +0800

    Update q2.py

--- specimenround1/q2.py
+++ specimenround1/q2.py
@@ -1,11 +1,17 @@
-n1 = input()
-if n1 < 0 or n1 > 1000:
-  continue
-n2 = int(input())
-if n2 <= 0 or n2 > 1000:
-  continue
-if n2 == n1:
-  continue
+while True:
+  n1 = input()
+  if n1 < 0 or n1 > 1000:
+    continue
+  else:
+    break
+while True:
+  n2 = int(input())
+  if n2 <= 0 or n2 > 1000:
+    continue
+  if n2 == n1:
+    continue
+  else:
+    break
 if n1>n2:
   print(n1-n2)
 else:
