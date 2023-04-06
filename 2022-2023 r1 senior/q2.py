a11b08c55c888b3acc9100e4af43bfbbd8dd2331
ddarlenee <125565155+ddarlenee@users.noreply.github.com>
2023-04-06 20:57:37 +0800

    Update q2.py

--- 2022-2023 r1 senior/q2.py
+++ 2022-2023 r1 senior/q2.py
@@ -2,14 +2,12 @@ while True:
     char1 = input()
     char2 = input()
     if len(char1) != 1 or len(char2) != 1:
-          continue
+        continue
     if char1.isdigit() or char2.isdigit():
-          continue
+        continue
     if char2 == char1:
         continue
     else:
         break
 print(char2*2 + char1*6 + char2*2)
 print(char1*2 + char2*6 + char1*2)
-    
-    
